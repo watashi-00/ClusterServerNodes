5c2337b303488ee7b53bf86804f81ba3f1c7758f
watashi-00 <kalitajj374@gmail.com>
2026-07-19 16:03:08 -0300

    Refactor ClusterController to delegate to ClusterService via TelemetryRequest DTO

--- java/src/hexacloud/core/utils/CharsetUtils.java
+++ java/src/hexacloud/core/utils/CharsetUtils.java
@@ -9,11 +9,15 @@ public enum CharsetUtils {
     ISO_8859_1;
 
     public static Charset resolve(CharsetUtils charset) {
-        return switch (charset) {
-            case UTF_8 -> StandardCharsets.UTF_8;
-            case US_ASCII -> StandardCharsets.US_ASCII;
-            case ISO_8859_1 -> StandardCharsets.ISO_8859_1;
-            default -> throw new IllegalArgumentException("Unsupported charset: " + charset);
-        };
+        switch (charset) {
+            case UTF_8:
+                return StandardCharsets.UTF_8;
+            case US_ASCII:
+                return StandardCharsets.US_ASCII;
+            case ISO_8859_1:
+                return StandardCharsets.ISO_8859_1;
+            default:
+                throw new IllegalArgumentException("Unsupported charset: " + charset);
+        }
     }
 }
