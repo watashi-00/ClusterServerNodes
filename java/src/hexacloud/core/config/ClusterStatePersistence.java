d0f6e1473f2d7c44128c3c4276e5110e5504b480
watashi-00 <kalitajj374@gmail.com>
2026-07-21 22:22:36 -0300

    feat: display TCP proxy status, routing mode, and expand dashboard layout

--- java/src/hexacloud/core/config/ClusterStatePersistence.java
+++ java/src/hexacloud/core/config/ClusterStatePersistence.java
@@ -2,7 +2,6 @@ package hexacloud.core.config;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
